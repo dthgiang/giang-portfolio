d8f3c78f538b1cf7714bdff4e77ca2f733b30aaa
dthgiang <duongthanhgiang.0108@gmail.com>
2024-11-03 01:04:32 +0700

    Update intro block

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,36 +1,55 @@
 import React from "react";
 import "../../styles/Home.css";
-import introBackground from "../../assets/images/intro_background.svg";
 import circlePlay from "../../assets/images/circle-play.svg";
 import textContent from "../../texts/textContent";
+import circleBase from "../../assets/images/base-yellow.svg";
+import introArrow from "../../assets/images/arrow-intro.svg";
+import whiteSign from "../../assets/images/white-sign.svg";
+import lineCircle from "../../assets/images/line-circle.svg";
 
 const Home = () => {
   return (
     <div>
-      <img
-        src={introBackground}
-        alt="introBackGround"
-        className="intro-background"
-      />
-      <div className="header">
-        <div className="logo-name">{textContent.logoName}</div>
-        <nav className="nav-menu">
-          <span className="nav-item">{textContent.home}</span>
-          <span className="nav-item">{textContent.about}</span>
-          <span className="nav-item">{textContent.services}</span>
-          <span className="nav-item">{textContent.projects}</span>
-          <span className="nav-item">{textContent.contact}</span>
-        </nav>
-        <div className="downloadCVButton">{textContent.downloadCV}</div>
-      </div>
-      <div className="introText">{textContent.introText}</div>
-      <div className="introSubText">{textContent.introSubText}</div>
-      <div className="buttonGroupIntro">
-        <div className="getStartedButton">{textContent.getStarted}</div>
-        <div className="watchDemoGroup">
-          <img src={circlePlay} alt="circlePlay" className="circle-play" />
+      {/* Intro Block */}
+      <div className="intro-block">
+        <div className="header">
+          <img src={lineCircle} alt="lineCircle" className="line-circle-left" />
+          <img src={introArrow} alt="introArrow" className="intro-arrow" />
+          <img src={whiteSign} alt="whiteSign" className="white-sign" />
+          <img src={circleBase} alt="circleBase" className="circle-base" />
+          <img
+            src={lineCircle}
+            alt="lineCircle"
+            className="line-circle-right"
+          />
+          <div className="logo-name">{textContent.logoName}</div>
+          <nav className="nav-menu">
+            <span className="nav-item">{textContent.home}</span>
+            <span className="nav-item">{textContent.about}</span>
+            <span className="nav-item">{textContent.services}</span>
+            <span className="nav-item">{textContent.projects}</span>
+            <span className="nav-item">{textContent.contact}</span>
+          </nav>
+          <div className="downloadCVButton">{textContent.downloadCV}</div>
         </div>
-        <div className="watchDemoGroupText">{textContent.watchDemo}</div>
+        <div className="introText">{textContent.introText}</div>
+        <div className="introSubText">{textContent.introSubText}</div>
+        <div className="buttonGroupIntro">
+          <div className="getStartedButton">{textContent.getStarted}</div>
+          <div className="watchDemoGroup">
+            <img src={circlePlay} alt="circlePlay" className="circle-play" />
+          </div>
+          <div className="watchDemoGroupText">{textContent.watchDemo}</div>
+        </div>
+      </div>
+
+      {/* Service Block */}
+      <div>
+        <div className="service-text">
+          {textContent.servicesText}
+          <div className="service-title">{textContent.servicesTitle}</div>
+          <div className="service-subtext">{textContent.servicesSubText}</div>
+        </div>
       </div>
     </div>
   );
--- src/texts/textContent.js
+++ src/texts/textContent.js
@@ -11,6 +11,10 @@ const textContent = {
     "I am dedicated to creating visually appealing designs that enhance usability and provide a memorable user experience.",
   getStarted: "Get Started",
   watchDemo: "Watch some demo",
+  servicesText: "SERVICES",
+  servicesTitle: "Expertise Service! Let's check it out",
+  servicesSubText:
+    "I offer design services focused on delivering visually engaging, user-friendly interfaces that enhance brand identity and improve overall user satisfaction.",
 };
 
 export default textContent;
