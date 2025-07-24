1318683c1d490ecca9ffb5747fcea0f3373d9b9d
dthgiang <duongthanhgiang.0108@gmail.com>
2025-07-25 06:53:28 +0700

    Update some Contact Page

--- src/pages/ContactPage/Contact.js
+++ src/pages/ContactPage/Contact.js
@@ -1,68 +1,23 @@
 import React from "react";
-import images from "../../constants/images";
 import "../../styles/Contact.css";
 import textContent from "../../constants/textContent";
-import { useNavigate } from "react-router-dom";
+import images from "../../constants/images";
 
 const Contact = () => {
-  const navigate = useNavigate();
-  const handleContactClick = () => {
-    navigate("/contact");
-  };
-  const handleAboutClick = () => {
-    navigate("/about");
-  };
-  const handleHomeClick = () => {
-    navigate("/");
-  };
   return (
-    <div className="header">
-      <div className="logo-header" onClick={handleHomeClick}>
-        {textContent.logoName}
+    <div className="contact-page">
+      <h1 className="contact-page-title">{textContent.contactTitle}</h1>
+      <p className="contact-page-content">{textContent.contactContent}</p>
+      <div className="contact-page-form">
+        <form>
+          <input type="text" placeholder="Your Name" required />
+          <input type="email" placeholder="Your Email" required />
+          <textarea placeholder="Your Message" required></textarea>
+          <button type="submit">Send Message</button>
+        </form>
       </div>
-      <nav className="nav-menu">
-        <span className="nav-item" onClick={handleAboutClick}>
-          {textContent.about}
-        </span>
-        <span className="nav-item" onClick={handleContactClick}>
-          {textContent.contact}
-        </span>
-      </nav>
-      <div className="social-icons">
-        <div className="gmail-wrapper">
-          <img src={images.gmailIcon} class="gmail-img" alt="Gmail" />
-          <img
-            src={images.gmailHeader}
-            class="gmail-img-hover"
-            alt="Gmail Hover"
-          />
-          <span className="email-text">{textContent.gmail}</span>
-        </div>
-        <a
-          href="https://github.com/dthgiang"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={images.githubWhite} alt="GitHub" className="github-img" />
-        </a>
-        <a
-          href="https://linkedin.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={images.linkedinIcon}
-            alt="LinkedIn"
-            className="linkedin-img"
-          />
-        </a>
-        <a
-          href="https://www.figma.com/@zangah"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={images.figmaFill} alt="Figma" className="figma-img" />
-        </a>
+      <div className="contact-page-footer">
+        <img src={images.real} alt="Contact Illustration" />
       </div>
     </div>
   );
