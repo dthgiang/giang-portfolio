0eab034da7b18db05a286db4bdc8cf679874ae7b
dthgiang <duongthanhgiang.0108@gmail.com>
2025-07-25 09:46:58 +0700

    logo contact page

--- src/pages/ContactPage/Contact.js
+++ src/pages/ContactPage/Contact.js
@@ -5,20 +5,10 @@ import images from "../../constants/images";
 
 const Contact = () => {
   return (
-    <div className="contact-page">
-      <h1 className="contact-page-title">{textContent.contactTitle}</h1>
-      <p className="contact-page-content">{textContent.contactContent}</p>
-      <div className="contact-page-form">
-        <form>
-          <input type="text" placeholder="Your Name" required />
-          <input type="email" placeholder="Your Email" required />
-          <textarea placeholder="Your Message" required></textarea>
-          <button type="submit">Send Message</button>
-        </form>
-      </div>
-      <div className="contact-page-footer">
-        <img src={images.real} alt="Contact Illustration" />
-      </div>
+    <div className="header-contact">
+      <div className="logo-contact-page">{textContent.logoName}</div>
+      <div>Menu</div>
+      <div>Icons</div>
     </div>
   );
 };
