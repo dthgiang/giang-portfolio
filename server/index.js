83dca36fd3b968f2379626febe82a459f25d98c6
dthgiang <duongthanhgiang.0108@gmail.com>
2025-08-11 12:52:06 +0700

    update package

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+<!-- const express = require("express");
 const cors = require("cors");
 const app = express();
 
@@ -12,4 +12,4 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-});
+}); -->
